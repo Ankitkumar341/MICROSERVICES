2cf8d165ca7d81f125d9f2d2bc78e696bbc1f1c2
Your Ankitkumar341 <gamerhumai.1@gmail.com>
2026-01-14 23:53:33 +0530

    Global Exception Handler Implemented

--- ADDRESS/src/main/java/com/address/exception/GlobalExceptionHandler.java
+++ ADDRESS/src/main/java/com/address/exception/GlobalExceptionHandler.java
@@ -1,29 +1,27 @@
 package com.address.exception;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.ErrorResponse;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
-//
-//    @ExceptionHandler(ResourceNotFoundException.class)
-//    public ResponseEntity<ErrorResponse> handleResourceNotFoundException (ResourceNotFoundException ex){
-//        ErrorResponse errorResponse = new ErrorResponse(ex.getMessage() , ex.getHttpStatus());
-//        return  new ResponseEntity<>(errorResponse,ex.getHttpStatus());
-//    }
-//
-//    @ExceptionHandler(BadRequestException.class)
-//    public ResponseEntity<ErrorResponse> handleResourceNotFoundException (BadRequestException ex){
-//        ErrorResponse errorResponse = new ErrorResponse(ex.getMessage() , ex.getHttpStatus());
-//        return  new ResponseEntity<>(errorResponse,ex.getHttpStatus());
-//    }
-//    @ExceptionHandler(MissingParameterExceptions.class)
-//    public ResponseEntity<ErrorResponse> handleMissingParameterExceptions (MissingParameterExceptions ex){
-//        ErrorResponse errorResponse = new ErrorResponse(ex.getMessage() , ex.getHttpStatus());
-//        return  new ResponseEntity<>(errorResponse, ex.getHttpStatus());
-//    }
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<ErrorResponse> handleResourceNotFoundException (ResourceNotFoundException ex){
+        ErrorResponse errorResponse = new ErrorResponse(ex.getMessage(), ex.getHttpStatus());
+        return  new ResponseEntity<>(errorResponse,ex.getHttpStatus());
+    }
+
+    @ExceptionHandler(BadRequestException.class)
+    public ResponseEntity<ErrorResponse> handleResourceNotFoundException (BadRequestException ex){
+        ErrorResponse errorResponse = new ErrorResponse(ex.getMessage() , ex.getHttpStatus());
+        return  new ResponseEntity<>(errorResponse,ex.getHttpStatus());
+    }
+    @ExceptionHandler(MissingParameterExceptions.class)
+    public ResponseEntity<ErrorResponse> handleMissingParameterExceptions (MissingParameterExceptions ex){
+        ErrorResponse errorResponse = new ErrorResponse(ex.getMessage() , ex.getHttpStatus());
+        return  new ResponseEntity<>(errorResponse, ex.getHttpStatus());
+    }
 
 }
